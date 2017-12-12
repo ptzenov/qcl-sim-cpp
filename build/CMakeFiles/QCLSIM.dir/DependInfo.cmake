58d127c1672f63ad37fac8260e540b1419e25cc6
ptzenov <petar.tzenov@transmetrics.eu>
2017-12-12 13:02:17 +0100

    some brushing up

--- build/CMakeFiles/QCLSIM.dir/DependInfo.cmake
+++ build/CMakeFiles/QCLSIM.dir/DependInfo.cmake
@@ -1,29 +1,27 @@
 # The set of languages for which implicit dependencies are needed:
-SET(CMAKE_DEPENDS_LANGUAGES
+set(CMAKE_DEPENDS_LANGUAGES
   "CXX"
   )
 # The set of files for implicit dependencies of each language:
-SET(CMAKE_DEPENDS_CHECK_CXX
-  "/home/petzko/REPOSITORIES/qcl-sim-cpp/src/common/utils.cpp" "/home/petzko/REPOSITORIES/qcl-sim-cpp/build/CMakeFiles/QCLSIM.dir/src/common/utils.cpp.o"
-  "/home/petzko/REPOSITORIES/qcl-sim-cpp/src/main.cpp" "/home/petzko/REPOSITORIES/qcl-sim-cpp/build/CMakeFiles/QCLSIM.dir/src/main.cpp.o"
-  "/home/petzko/REPOSITORIES/qcl-sim-cpp/src/matrix/matrix.cpp" "/home/petzko/REPOSITORIES/qcl-sim-cpp/build/CMakeFiles/QCLSIM.dir/src/matrix/matrix.cpp.o"
-  "/home/petzko/REPOSITORIES/qcl-sim-cpp/src/parsers/SimFileParser.cpp" "/home/petzko/REPOSITORIES/qcl-sim-cpp/build/CMakeFiles/QCLSIM.dir/src/parsers/SimFileParser.cpp.o"
-  "/home/petzko/REPOSITORIES/qcl-sim-cpp/src/sim/SimLoop.cpp" "/home/petzko/REPOSITORIES/qcl-sim-cpp/build/CMakeFiles/QCLSIM.dir/src/sim/SimLoop.cpp.o"
-  "/home/petzko/REPOSITORIES/qcl-sim-cpp/src/sim/SimSettings.cpp" "/home/petzko/REPOSITORIES/qcl-sim-cpp/build/CMakeFiles/QCLSIM.dir/src/sim/SimSettings.cpp.o"
-  "/home/petzko/REPOSITORIES/qcl-sim-cpp/src/solvers/MultistepDMSolver.cpp" "/home/petzko/REPOSITORIES/qcl-sim-cpp/build/CMakeFiles/QCLSIM.dir/src/solvers/MultistepDMSolver.cpp.o"
-  "/home/petzko/REPOSITORIES/qcl-sim-cpp/src/solvers/RNFDSolver.cpp" "/home/petzko/REPOSITORIES/qcl-sim-cpp/build/CMakeFiles/QCLSIM.dir/src/solvers/RNFDSolver.cpp.o"
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/kenny/Documents/workspaces/githubrepos/qcl-sim-cpp/src/common/utils.cpp" "/home/kenny/Documents/workspaces/githubrepos/qcl-sim-cpp/build/CMakeFiles/QCLSIM.dir/src/common/utils.cpp.o"
+  "/home/kenny/Documents/workspaces/githubrepos/qcl-sim-cpp/src/main.cpp" "/home/kenny/Documents/workspaces/githubrepos/qcl-sim-cpp/build/CMakeFiles/QCLSIM.dir/src/main.cpp.o"
+  "/home/kenny/Documents/workspaces/githubrepos/qcl-sim-cpp/src/parsers/SimFileParser.cpp" "/home/kenny/Documents/workspaces/githubrepos/qcl-sim-cpp/build/CMakeFiles/QCLSIM.dir/src/parsers/SimFileParser.cpp.o"
+  "/home/kenny/Documents/workspaces/githubrepos/qcl-sim-cpp/src/sim/SimLoop.cpp" "/home/kenny/Documents/workspaces/githubrepos/qcl-sim-cpp/build/CMakeFiles/QCLSIM.dir/src/sim/SimLoop.cpp.o"
+  "/home/kenny/Documents/workspaces/githubrepos/qcl-sim-cpp/src/sim/SimSettings.cpp" "/home/kenny/Documents/workspaces/githubrepos/qcl-sim-cpp/build/CMakeFiles/QCLSIM.dir/src/sim/SimSettings.cpp.o"
+  "/home/kenny/Documents/workspaces/githubrepos/qcl-sim-cpp/src/solvers/MultistepDMSolver.cpp" "/home/kenny/Documents/workspaces/githubrepos/qcl-sim-cpp/build/CMakeFiles/QCLSIM.dir/src/solvers/MultistepDMSolver.cpp.o"
+  "/home/kenny/Documents/workspaces/githubrepos/qcl-sim-cpp/src/solvers/RNFDSolver.cpp" "/home/kenny/Documents/workspaces/githubrepos/qcl-sim-cpp/build/CMakeFiles/QCLSIM.dir/src/solvers/RNFDSolver.cpp.o"
   )
-SET(CMAKE_CXX_COMPILER_ID "GNU")
+set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # Targets to which this target links.
-SET(CMAKE_TARGET_LINKED_INFO_FILES
+set(CMAKE_TARGET_LINKED_INFO_FILES
   )
 
 # The include file search paths:
-SET(CMAKE_C_TARGET_INCLUDE_PATH
+set(CMAKE_C_TARGET_INCLUDE_PATH
   "../include"
-  "/mnt/SHARED/Matlab_Linux/extern/include"
   )
-SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
-SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
-SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
--- build/CMakeFiles/QCLSIM.dir/cmake_clean.cmake
+++ build/CMakeFiles/QCLSIM.dir/cmake_clean.cmake
@@ -1,17 +1,16 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/QCLSIM.dir/src/sim/SimLoop.cpp.o"
-  "CMakeFiles/QCLSIM.dir/src/sim/SimSettings.cpp.o"
-  "CMakeFiles/QCLSIM.dir/src/solvers/MultistepDMSolver.cpp.o"
-  "CMakeFiles/QCLSIM.dir/src/solvers/RNFDSolver.cpp.o"
-  "CMakeFiles/QCLSIM.dir/src/parsers/SimFileParser.cpp.o"
-  "CMakeFiles/QCLSIM.dir/src/matrix/matrix.cpp.o"
-  "CMakeFiles/QCLSIM.dir/src/common/utils.cpp.o"
+file(REMOVE_RECURSE
   "CMakeFiles/QCLSIM.dir/src/main.cpp.o"
+  "CMakeFiles/QCLSIM.dir/src/parsers/SimFileParser.cpp.o"
+  "CMakeFiles/QCLSIM.dir/src/common/utils.cpp.o"
+  "CMakeFiles/QCLSIM.dir/src/sim/SimSettings.cpp.o"
+  "CMakeFiles/QCLSIM.dir/src/sim/SimLoop.cpp.o"
+  "CMakeFiles/QCLSIM.dir/src/solvers/RNFDSolver.cpp.o"
+  "CMakeFiles/QCLSIM.dir/src/solvers/MultistepDMSolver.cpp.o"
   "QCLSIM.pdb"
   "QCLSIM"
 )
 
 # Per-language clean rules from dependency scanning.
-FOREACH(lang CXX)
-  INCLUDE(CMakeFiles/QCLSIM.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
+foreach(lang CXX)
+  include(CMakeFiles/QCLSIM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
